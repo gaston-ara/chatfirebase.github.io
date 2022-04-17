79208700a2f4ce0b6083b3f9e78af0b548e17edd
gaston.ara <gaston.ara.o@gmail.com>
2022-04-16 23:36:49 -0300

    Estilos del texto corregidos

--- src/components/ChatInputs.js
+++ src/components/ChatInputs.js
@@ -4,7 +4,7 @@ const ChatInputs = (props) => {
   return (
     <>
     <form className='form' onSubmit={(e) => props.handleSubmit(e)}>
-        <input className='enter-message' value={props.message} type="text" placeholder="Type a message..." onChange={(e) => props.setMessage(e.target.value)}/>
+        <input className='enter-message' value={props.message} type="text" placeholder="Escribe tu mensaje..." onChange={(e) => props.setMessage(e.target.value)}/>
         <button className='btn' type="submit">Send</button>
     </form>
     </>
